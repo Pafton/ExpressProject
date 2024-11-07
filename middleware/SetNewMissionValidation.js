b58311057e05077d6e11c8c4556f6667c6c55e55
Pafton <ptrojnar3@gmail.com>
2024-11-07 19:24:23 +0100

    Validation setting change

--- middleware/SetNewMissionValidation.js
+++ middleware/SetNewMissionValidation.js
@@ -3,15 +3,15 @@ module.exports = function validateCrewFormat(req, res, next) {
     let error = null;
 
     if (!crew) {
-        error = "Błąd: Pole 'crew' jest wymagane.";
+        error = "Błąd: Pole 'Załoga' jest wymagane.";
     } else {
-        const crewMembers = crew.split(';');
-        const regex = /^[A-Za-z]+\s[A-Za-z]+\s-\s[A-Za-z]+$/;
+        const crewMembers = crew.split(',');
+        const regex = /^[A-Za-z]+(\s"[A-Za-z\s]+")?\s[A-Za-z]+\s-\s[A-Za-z]+$/;
 
         for (const member of crewMembers) {
             if (!regex.test(member.trim())) {
-                error = "Błąd: Format danych w polu 'crew' jest nieprawidłowy. Użyj formatu 'Imię Nazwisko - Rola , ...'.";
-                break; 
+                error = "Błąd: Format danych w polu 'Zaloga' jest nieprawidłowy. Użyj formatu 'Imię Nazwisko - Rola; Imię Nazwisko - Rola; ...'.";
+                break;
             }
         }
     }
